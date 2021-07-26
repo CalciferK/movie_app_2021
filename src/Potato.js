d4bed3ea152b2bf61b489901a632ba32a33e0efa
gimsein <continue.deve@gmail.com>
2021-07-26 14:30:48 +0900

    #1 the first pratice

---
+++ src/Potato.js
@@ -0,0 +1,9 @@
+import React from "react"; //선언하지 않으면 react는 여기에 jsx가 있는 componet를 사용하는 것을 이해하지 못한다.
+
+function Potato() {
+    return (
+        <h3>I love potato </h3>
+    )
+}
+
+export default Potato;
